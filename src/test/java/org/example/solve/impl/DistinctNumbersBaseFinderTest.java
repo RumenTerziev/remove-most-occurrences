458ff3854c248a4639ddd45597fd318e652e9ce6
Rumen Terziev <106109989+RumenTerziev@users.noreply.github.com>
2024-04-20 16:34:46 +0300

    Added test case.

--- src/test/java/org/example/solve/impl/DistinctNumbersBaseFinderTest.java
+++ src/test/java/org/example/solve/impl/DistinctNumbersBaseFinderTest.java
@@ -59,4 +59,30 @@ class DistinctNumbersBaseFinderTest {
         //Then
         assertEquals(expected, actual);
     }
+
+    @Test
+    void testGetDistinctNumbersCount_whenGivenDifferentNumberOfOccurrences_shouldPrioritiseTheGraterNumberOfOccurrences() {
+        //Given
+        int countRemovals = 6;
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(4);
+        numbers.add(3);
+        numbers.add(3);
+        numbers.add(2);
+        numbers.add(2);
+        numbers.add(2);
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(1);
+        InputData inputData = new InputData(numbers, countRemovals);
+        int expected = 3;
+
+        //When
+        int actual = distinctNumbersFinder.getDistinctNumbersCount(inputData);
+
+        //Then
+        assertEquals(expected, actual);
+        assert(numbers).contains(3);
+    }
 }
